f8d8ed81b17c7f391f854417b94962d130cbcd9c
AiswaryaaRajesh <72779223+AiswaryaaRajesh@users.noreply.github.com>
2024-04-17 09:46:48 +0530

    Update script.js sidepanel length

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ window.addEventListener('load', () => {
 
 //Sidepanel
 function openNav() {
-    document.getElementById("mySidepanel").style.width = "100%";
+    document.getElementById("mySidepanel").style.width = "80%";
 }
 
 function closeNav() {
